af283f47e0d4e832157f9216a4a4d7fd23e8f01c
unknown <elaniobros@gmail.com>
2024-05-21 19:04:32 -0300

    add sprites and fix scale phase

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const config = {
   type: Phaser.AUTO,
   scale: {
-    mode: Phaser.Scale.AUTO,
+    mode: Phaser.Scale.RESIZE,
     parent: "game",
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
